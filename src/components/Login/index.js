cb436e41c97fbd0436b503e154166b5ec39efa49
bojoyzhou <453398480@qq.com>
2016-06-13 16:25:23 +0800

    update

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Router, Route, IndexRoute } from 'react-router'
-
+import { VelocityComponent } from 'velocity-react'
 import style from './style.less'
 import { LOGIN_DIALOG, REGIST_DIALOG, FORGOT_DIALOG, REGIST_DIALOG_1, REGIST_DIALOG_2 } from '../../constants'
 
@@ -112,90 +112,94 @@ class Login extends Component {
     render() {
         const { isOpened, status, data, step, codelink } = this.props
         const className = isOpened ? "container-login open" : "container-login close"
+        const animation = "fade" + (isOpened ? "In" : "Out")
+        console.log(animation)
         return (
-            <div className={className}>
-                <div className="mask">
-                    <div className={"mask-panel " + status.toLowerCase()}>
-                        {
-                            (function(){
-                                if(status == LOGIN_DIALOG){
-                                    return (
-                                        <div className="mask-title clear">
-                                            <h4>登录</h4>
-                                            <span>还没有账号？<a onClick={this.handleRegist.bind(this)}>立即注册</a></span>
-                                            <a onClick={this.close.bind(this)} className="close">&times;</a>
-                                        </div>
-                                    )
-                                }else if(status == REGIST_DIALOG){
-                                    return (
-                                        <div className="mask-title clear">
-                                            <h4>注册</h4>
-                                            <span>已有账号，<a onClick={this.handleLogin.bind(this)}>直接登录</a></span>
-                                            <a onClick={this.close.bind(this)} href="javascript:;" className="close">&times;</a>
-                                        </div>
-                                    )
-                                }
-                            }).call(this)
-                        }
+            <VelocityComponent key="Fade" animation={animation}>
+                <div className="container-login">
+                    <div className="mask">
+                        <div className={"mask-panel " + status.toLowerCase()}>
+                            {
+                                (function(){
+                                    if(status == LOGIN_DIALOG){
+                                        return (
+                                            <div className="mask-title clear">
+                                                <h4>登录</h4>
+                                                <span>还没有账号？<a onClick={this.handleRegist.bind(this)}>立即注册</a></span>
+                                                <a onClick={this.close.bind(this)} className="close">&times;</a>
+                                            </div>
+                                        )
+                                    }else if(status == REGIST_DIALOG){
+                                        return (
+                                            <div className="mask-title clear">
+                                                <h4>注册</h4>
+                                                <span>已有账号，<a onClick={this.handleLogin.bind(this)}>直接登录</a></span>
+                                                <a onClick={this.close.bind(this)} href="javascript:;" className="close">&times;</a>
+                                            </div>
+                                        )
+                                    }
+                                }).call(this)
+                            }
 
-                        <div className="mask-body clear">
-                            <div className="form login-form" style={{ display: status == LOGIN_DIALOG ? "block" : "none"}}>
-                                <label className="email">
-                                    <input type="text" onChange={(e) => this.handleChange('email', e.target.value)} value={data.email} placeholder="请输入您的用户名" />
-                                </label>
-                                <label className="password">
-                                    <input type="password" onChange={(e) => this.handleChange('password', e.target.value)} value={data.password} placeholder="请输入您的密码" />
-                                </label>
-                                <label className="vcode">
-                                    <input type="text" onChange={(e) => this.handleChange('vcode', e.target.value)} value={data.vcode} placeholder="请输入验证码" />
-                                    <img className="vcode-img" onClick={this.changeVcode.bind(this)} src={codelink} alt=""/>
-                                </label>
-                            </div>
-                            <div className="form regist-form" style={{ display: status == REGIST_DIALOG ? "block" : "none"}}>
-                                <div className="step-1" style={{ display: step == REGIST_DIALOG_1 ? "block" : "none"}}>
+                            <div className="mask-body clear">
+                                <div className="form login-form" style={{ display: status == LOGIN_DIALOG ? "block" : "none"}}>
                                     <label className="email">
-                                        <input type="text" onChange={(e) => this.handleChange('email', e.target.value)} value={data.email} placeholder="请输入您的常用邮箱" />
+                                        <input type="text" onChange={(e) => this.handleChange('email', e.target.value)} value={data.email} placeholder="请输入您的用户名" />
+                                    </label>
+                                    <label className="password">
+                                        <input type="password" onChange={(e) => this.handleChange('password', e.target.value)} value={data.password} placeholder="请输入您的密码" />
                                     </label>
                                     <label className="vcode">
                                         <input type="text" onChange={(e) => this.handleChange('vcode', e.target.value)} value={data.vcode} placeholder="请输入验证码" />
                                         <img className="vcode-img" onClick={this.changeVcode.bind(this)} src={codelink} alt=""/>
                                     </label>
                                 </div>
-                                <div className="step-2" style={{ display: step == REGIST_DIALOG_2 ? "block" : "none"}}>
-                                    <label className="username">
-                                        <input type="text" onChange={(e) => this.handleChange('uname', e.target.value)} value={data.uname} placeholder="请输入您的昵称" />
+                                <div className="form regist-form" style={{ display: status == REGIST_DIALOG ? "block" : "none"}}>
+                                    <div className="step-1" style={{ display: step == REGIST_DIALOG_1 ? "block" : "none"}}>
+                                        <label className="email">
+                                            <input type="text" onChange={(e) => this.handleChange('email', e.target.value)} value={data.email} placeholder="请输入您的常用邮箱" />
+                                        </label>
+                                        <label className="vcode">
+                                            <input type="text" onChange={(e) => this.handleChange('vcode', e.target.value)} value={data.vcode} placeholder="请输入验证码" />
+                                            <img className="vcode-img" onClick={this.changeVcode.bind(this)} src={codelink} alt=""/>
+                                        </label>
+                                    </div>
+                                    <div className="step-2" style={{ display: step == REGIST_DIALOG_2 ? "block" : "none"}}>
+                                        <label className="username">
+                                            <input type="text" onChange={(e) => this.handleChange('uname', e.target.value)} value={data.uname} placeholder="请输入您的昵称" />
+                                        </label>
+                                        <label className="password">
+                                            <input type="password" onChange={(e) => this.handleChange('password', e.target.value)} value={data.password} placeholder="请输入您的密码" />
+                                        </label>
+                                        <label className="password">
+                                            <input type="password" onChange={(e) => this.handleChange('repassword', e.target.value)} value={data.repassword} placeholder="请再次输入您的密码" />
+                                        </label>
+                                    </div>
+                                </div>
+                                <div className="form-tail" style={{ display: status == LOGIN_DIALOG ? "block" : "none"}}>
+                                    <label for="remember">
+                                        <input type="checkbox" />记住密码
                                     </label>
-                                    <label className="password">
-                                        <input type="password" onChange={(e) => this.handleChange('password', e.target.value)} value={data.password} placeholder="请输入您的密码" />
-                                    </label>
-                                    <label className="password">
-                                        <input type="password" onChange={(e) => this.handleChange('repassword', e.target.value)} value={data.repassword} placeholder="请再次输入您的密码" />
+                                    <label for="autologin">
+                                        <input type="checkbox" />自动登录
                                     </label>
                                 </div>
                             </div>
-                            <div className="form-tail" style={{ display: status == LOGIN_DIALOG ? "block" : "none"}}>
-                                <label for="remember">
-                                    <input type="checkbox" />记住密码
-                                </label>
-                                <label for="autologin">
-                                    <input type="checkbox" />自动登录
-                                </label>
+                            <div className="mask-footer" style={{ display: status == LOGIN_DIALOG ? "block" : "none"}}>
+                                <button onClick={this.doLogin.bind(this)} className="btn btn-block">登录</button>
+                                <a href="">忘记密码</a>
                             </div>
-                        </div>
-                        <div className="mask-footer" style={{ display: status == LOGIN_DIALOG ? "block" : "none"}}>
-                            <button onClick={this.doLogin.bind(this)} className="btn btn-block">登录</button>
-                            <a href="">忘记密码</a>
-                        </div>
-                        <div className="mask-footer" style={{ display: status == REGIST_DIALOG ? "block" : "none"}}>
-                            <button onClick={this.doRegist.bind(this)} className="btn btn-block">
-                                {
-                                    step == REGIST_DIALOG_1 ? '下一步' : '注册'
-                                }
-                            </button>
+                            <div className="mask-footer" style={{ display: status == REGIST_DIALOG ? "block" : "none"}}>
+                                <button onClick={this.doRegist.bind(this)} className="btn btn-block">
+                                    {
+                                        step == REGIST_DIALOG_1 ? '下一步' : '注册'
+                                    }
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </VelocityComponent>
         )
     }
 }
